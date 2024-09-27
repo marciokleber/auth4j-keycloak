d79f284bbbd3f27d0e79d7eff5613ad596cfbbc4
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2024-09-27 16:14:34 -0300

    Initial commit

---
+++ src/main/java/com/github/marciokleber/auth4jkeycloak/Auth4jKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.github.marciokleber.auth4jkeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth4jKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Auth4jKeycloakApplication.class, args);
+	}
+
+}
